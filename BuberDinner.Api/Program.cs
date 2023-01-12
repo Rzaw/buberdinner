600e81771c7d8eab394197cfeffc51ee4d839832
Matijs Lode <matijs.lode@lak4.lv>
2023-01-12 13:41:17 +0200

    Creating API endpoints for auth service. Register Application, Infrastructure dependencies to DI

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,7 +1,13 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
     builder.Services.AddControllers();
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
 }
 
 var app = builder.Build();
