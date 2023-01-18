6a971235388accf0310991c8a64d12915377af5a
Matijs Lode <matijs.lode@lak4.lv>
2023-01-18 15:01:46 +0200

    Refactoring authentification controller

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -25,10 +25,10 @@ public class AuthenticationController : ControllerBase
             request.Password);
         
         var response = new AuthenticationResponse(
-            result.Id, 
-            result.FirstName, 
-            result.LastName, 
-            result.Email, 
+            result.User.Id, 
+            result.User.FirstName, 
+            result.User.LastName, 
+            result.User.Email, 
             result.Token);
         return Ok(response);
     }
@@ -37,7 +37,12 @@ public class AuthenticationController : ControllerBase
     public IActionResult Login(LoginRequest request)
     {
         var result = _authenticationService.Login(request.Email, request.Password);
-        var response = new AuthenticationResponse(result.Id, result.FirstName, result.LastName, result.Email, result.Token);
+        var response = new AuthenticationResponse(
+            result.User.Id, 
+            result.User.FirstName, 
+            result.User.LastName, 
+            result.User.Email, 
+            result.Token);
         return Ok(response);
     }
 }
