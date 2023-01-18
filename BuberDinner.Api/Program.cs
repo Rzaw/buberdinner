462625d5c05645a67d8d8ce45ea19dae38303baa
Matijs Lode <matijs.lode@lak4.lv>
2023-01-18 16:12:29 +0200

    Explored different ways how to handle exception

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,17 +1,24 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
+    // builder.Services.AddControllers(opt => opt.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemErrorsFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 }
